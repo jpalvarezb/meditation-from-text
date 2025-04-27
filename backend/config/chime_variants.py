554c06046b0a55d982bca715579acc7d5f837c9d
JP Alvarez <jpabalto11@gmail.com>
2025-04-27 17:00:07 -0400

    created and developed chime variants/trigger word configurations

---
+++ backend/config/chime_variants.py
@@ -0,0 +1,10 @@
+BAR_CHIME_VARIANTS = [
+    "bar_chime_1.wav",
+    "bar_chime_2.wav",
+    "bar_chime_3.wav",
+    "bar_chime_4.wav",
+    "bar_chime_5.wav",
+]
+
+# Different chime variants that rotate during the meditation session.
+# They're used to bring the user back to the present moment.
